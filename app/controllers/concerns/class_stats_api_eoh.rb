254b13707931836246bcaa043be3807425360e90
k0chame-lenovo <k0chame@>
2019-03-03 18:53:32 +0900

    classのendが抜けていたので修正

--- app/controllers/concerns/class_stats_api_eoh.rb
+++ app/controllers/concerns/class_stats_api_eoh.rb
@@ -2,7 +2,7 @@
 
 #家計消費支出(expenditure of households)
 class Api_eoh < Estat_api
- 
+
   #家計消費支出＿年次 ※年次，四半期はデータ構成が違うので注意！
   def update_eoh_calyear(db_datas,stats_id)
     read_cat01_code = "059"
@@ -13,19 +13,19 @@ class Api_eoh < Estat_api
     stats_id.each do |id|
       # ＵＲＬ作成～統計リスト作成のメソッドを呼び出し
       update_statistics_list(id)
-  
+
       # 必要な項目をそれぞれ変数に代入
       @stat_code = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:TABLE_INF][:STAT_NAME][:@code]
       @stat_name = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:TABLE_INF][:STAT_NAME][:"$"]
       @table_code = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:TABLE_INF][:@id]
       @table_name = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:TABLE_INF][:STATISTICS_NAME]
       @update_date = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:TABLE_INF][:UPDATED_DATE]
-      
-      
+
+
       # 統計リストＤＢの作成
       db_statlists = StatisticsList.all
       statlist = db_statlists.find_by(table_code:@table_code)
-  
+
       if statlist == nil
         db_statlists.create(
           stat_code:@stat_code,
@@ -37,23 +37,23 @@ class Api_eoh < Estat_api
       else
         statlist.stat_code = @stat_code
         statlist.stat_name = @stat_name
-        statlist.table_code = @table_code      
+        statlist.table_code = @table_code
         statlist.table_name = @table_name
         statlist.last_date = statlist.update_date
         statlist.update_date = @update_date
         statlist.save
       end
-      
+
       db_statlists = StatisticsList.all
         statlist = db_statlists.find_by(table_code:id)
-      
+
       # 統計リストの前回更新日が空欄、もしくは更新日が前回更新日と一致しない場合に
       # 統計ＤＢを作成、更新する
       if statlist.last_date == nil or statlist.last_date != statlist.update_date
 
           data_value = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:DATA_INF][:VALUE]
           data_classobj = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:CLASS_INF][:CLASS_OBJ]
-          
+
           db_datelists = DateList.all
           db_catlists = CategoryList.all
 
@@ -62,7 +62,7 @@ class Api_eoh < Estat_api
               when "time" then
                 data_time = obj[:CLASS]
                 date_unit = obj[:@name]
-                
+
                 data_time.each do |time|
                   if  db_datelists.find_by(date_code:time[:@code]) == nil
                       db_datelists.create(
@@ -74,9 +74,9 @@ class Api_eoh < Estat_api
                 end
               when "cat01" then
                 data_cat01 = obj[:CLASS]
-                
+
                 data_cat01.each do |cat01|
-                  if cat01[:@code] == read_cat01_code and db_catlists.find_by(category_code:cat01[:@code]) == nil 
+                  if cat01[:@code] == read_cat01_code and db_catlists.find_by(category_code:cat01[:@code]) == nil
                      db_catlists.create(
                          category_code:cat01[:@code],
                          category_name:cat01[:@name]
@@ -85,7 +85,7 @@ class Api_eoh < Estat_api
                 end
             end
           end
-          
+
           data_value.each do |data|
             if data[:@cat01] == read_cat01_code and data[:@cat02] == read_cat02_code and data[:@cat03] == read_cat03_code and data[:@area] == read_area_code
               db_datas.create(
@@ -99,7 +99,7 @@ class Api_eoh < Estat_api
             end
           end
       end
-    end  
+    end
   end
 
   #家計消費支出＿四半期(expenditure of households)　※年次，四半期はデータ構成が違うので注意！
@@ -111,19 +111,19 @@ class Api_eoh < Estat_api
     stats_id.each do |id|
       # ＵＲＬ作成～統計リスト作成のメソッドを呼び出し
       update_statistics_list(id)
-  
+
       # 必要な項目をそれぞれ変数に代入
       @stat_code = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:TABLE_INF][:STAT_NAME][:@code]
       @stat_name = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:TABLE_INF][:STAT_NAME][:"$"]
       @table_code = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:TABLE_INF][:@id]
       @table_name = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:TABLE_INF][:STATISTICS_NAME]
       @update_date = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:TABLE_INF][:UPDATED_DATE]
-      
-      
+
+
       # 統計リストＤＢの作成
       db_statlists = StatisticsList.all
       statlist = db_statlists.find_by(table_code:@table_code)
-  
+
       if statlist == nil
         db_statlists.create(
           stat_code:@stat_code,
@@ -135,23 +135,23 @@ class Api_eoh < Estat_api
       else
         statlist.stat_code = @stat_code
         statlist.stat_name = @stat_name
-        statlist.table_code = @table_code      
+        statlist.table_code = @table_code
         statlist.table_name = @table_name
         statlist.last_date = statlist.update_date
         statlist.update_date = @update_date
         statlist.save
       end
-      
+
       db_statlists = StatisticsList.all
         statlist = db_statlists.find_by(table_code:id)
-      
+
       # 統計リストの前回更新日が空欄、もしくは更新日が前回更新日と一致しない場合に
       # 統計ＤＢを作成、更新する
       if statlist.last_date == nil or statlist.last_date != statlist.update_date
 
           data_value = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:DATA_INF][:VALUE]
           data_classobj = @data_all[:GET_STATS_DATA][:STATISTICAL_DATA][:CLASS_INF][:CLASS_OBJ]
-          
+
           db_datelists = DateList.all
           db_catlists = CategoryList.all
 
@@ -160,7 +160,7 @@ class Api_eoh < Estat_api
               when "time" then
                 data_time = obj[:CLASS]
                 date_unit = obj[:@name]
-                
+
                 data_time.each do |time|
                   if  db_datelists.find_by(date_code:time[:@code]) == nil
                       db_datelists.create(
@@ -172,9 +172,9 @@ class Api_eoh < Estat_api
                 end
               when "cat01" then
                 data_cat01 = obj[:CLASS]
-                
+
                 data_cat01.each do |cat01|
-                  if cat01[:@code] == read_cat01_code and db_catlists.find_by(category_code:cat01[:@code]) == nil 
+                  if cat01[:@code] == read_cat01_code and db_catlists.find_by(category_code:cat01[:@code]) == nil
                      db_catlists.create(
                          category_code:cat01[:@code],
                          category_name:cat01[:@name]
@@ -183,7 +183,7 @@ class Api_eoh < Estat_api
                 end
             end
           end
-          
+
           data_value.each do |data|
             if data[:@cat01] == read_cat01_code and data[:@cat02] == read_cat02_code and data[:@area] == read_area_code
               db_datas.create(
@@ -197,5 +197,6 @@ class Api_eoh < Estat_api
             end
           end
       end
-    end  
+    end
   end
+end
